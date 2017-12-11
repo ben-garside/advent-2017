4c607e68f676d77210317d4da476f212a31fbe02
Benjamin Garside <abgarside@gmail.com>
2017-12-11 17:17:04 +0000

    Update benjamin-1.py

--- 10/benjamin-1.py
+++ 10/benjamin-1.py
@@ -6,14 +6,12 @@ def checklen(length, amount):
     if amount >= length:
         amount = amount - length
         return checklen(length, amount)
-    else:
-        return amount
+    return amount
 
 INPUT = "225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110"
 LENS = [int(x.strip()) for x in INPUT.split(',')]
 LIST = [int(x) for x in range(0, 256)]
 SKIP = 0
-CURRENT_LEN_INDEX = -1
 CURR = 0
 
 for length in LENS:
@@ -28,7 +26,7 @@ for length in LENS:
     for n in range(0, length):
         indx = checklen(len(LIST), CURR + n)
         LIST[indx] = new_values[n]
-    
+
     CURR =+ CURR + length + SKIP
     SKIP =+ SKIP + 1
 
